a7b35a5fc5d542531b83f81431b704bae39de318
Joshua Han <joshhan619@gmail.com>
2024-11-25 13:02:55 -0600

    chg: Use custom statespace NeedleSpace with turning direction included instead of just SE2 space

--- src/ProjectG.cpp
+++ src/ProjectG.cpp
@@ -1,5 +1,6 @@
 #include <ompl/base/SpaceInformation.h>
 #include <ompl/base/spaces/SE2StateSpace.h>
+#include <ompl/base/StateSpace.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <ompl/util/RandomNumbers.h>
 #include <vector>
@@ -36,9 +37,49 @@ struct Graph {
     std::vector<Edge> edges;
 };
 
+class NeedleStateSpace: public ob::CompoundStateSpace
+{
+    public:
+    NeedleStateSpace()
+    {
+        addSubspace(std::make_shared<ob::SE2StateSpace>(), 1.0);
+        // Set the weight of the orientation theta to alpha
+        addSubspace(std::make_shared<ob::RealVectorStateSpace>(1), 1.0);
+        getSubspaces()[0]->as<ob::SE2StateSpace>()->setSubspaceWeight(1, alpha);
+    }
+
+    double distance(const ob::State *state1, const ob::State *state2) const
+    {
+        const auto *cstate1 = static_cast<const ob::CompoundState *>(state1);
+        const auto *cstate2 = static_cast<const ob::CompoundState *>(state2);
+        double se2_dist = components_[0]->distance(cstate1->components[0], cstate2->components[0]);
+        double M = 0;
+
+        double b1 = cstate1->as<ob::RealVectorStateSpace::StateType>(1)->values[0];
+        double b2 = cstate2->as<ob::RealVectorStateSpace::StateType>(1)->values[0];
+        if (b1 != b2) {
+            M = 1;
+        }
+        return se2_dist + M;
+    }
+
+    protected:
+    double alpha{0.2}; // Default hyperparameter
+};
+
+// Assuming state is of type NeedleSpace::StateType
+std::tuple<double, double, double> getCoord(const ob::State *state) {
+    auto *needleState = state->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0);
+    double x = needleState->getX();
+    double y = needleState->getY();
+    double theta = needleState->getYaw();
+
+    return std::make_tuple(x, y, theta);
+}
+
 bool isCollisionFree(const ob::State *state) {
-    double x = state->as<ob::SE2StateSpace::StateType>()->getX();
-    double y = state->as<ob::SE2StateSpace::StateType>()->getY();
+    double x, y;
+    std::tie(x, y, std::ignore) = getCoord(state);
 
     // Check for going out of bounds
     if (x < -0.5 || x > 0.5 || y < -0.5 || y > 0.5) {
@@ -56,16 +97,16 @@ bool isCollisionFree(const ob::State *state) {
 }
 
 void propagateCircularArc(const ob::State *s, const int u, const double r, const double delta, ob::State *q) {
-    auto sState = s->as<ob::SE2StateSpace::StateType>();
-    double theta = sState->getYaw();
+    double x, y, theta;
+    std::tie(x, y, theta) = getCoord(s);
     int sign = (u == 0) ? 1 : -1;
     double theta_new = theta + sign * delta / r;
-    double x_new = sState->getX() - sign * r * std::sin(theta) + sign * r * std::sin(theta_new); 
-    double y_new = sState->getY() + sign * r * std::cos(theta) - sign * r * std::cos(theta_new);
+    double x_new = x - sign * r * std::sin(theta) + sign * r * std::sin(theta_new); 
+    double y_new = y + sign * r * std::cos(theta) - sign * r * std::cos(theta_new);
 
-    q->as<ob::SE2StateSpace::StateType>()->setX(x_new);
-    q->as<ob::SE2StateSpace::StateType>()->setY(y_new);
-    q->as<ob::SE2StateSpace::StateType>()->setYaw(theta_new);
+    q->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setX(x_new);
+    q->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setY(y_new);
+    q->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setYaw(theta_new);
 }
 
 bool isCollisionFreePath(const ob::State *s1, const ob::State *s2, const int u, const double r, const double delta, ob::SpaceInformationPtr si)
@@ -183,17 +224,11 @@ std::unordered_map<int, Graph> buildSMR(ob::SpaceInformationPtr si, ob::State *s
     return SMR;
 }
 
-bool isGoalReachable(ob::State *q, ob::State *goal, double radius) {
-    double qX = q->as<ob::SE2StateSpace::StateType>()->getX();
-    double qY = q->as<ob::SE2StateSpace::StateType>()->getY();
-
-    double goalX = goal->as<ob::SE2StateSpace::StateType>()->getX();
-    double goalY = goal->as<ob::SE2StateSpace::StateType>()->getY();
-
-    return pow(qX-goalX, 2) + pow(qY-goalY, 2) <= pow(radius, 2);
+bool isGoalReachable(ob::State *q, ob::State *goal, double radius, ob::SpaceInformationPtr si) {
+    return si->distance(q, goal) <= radius;
 }
 
-std::tuple<std::unordered_map<ob::State *, int>, std::unordered_map<ob::State *, double>> querySMR(std::unordered_map<int, Graph> SMR, ob::State *goal, ob::State *OBS_STATE, double radius) {
+std::tuple<std::unordered_map<ob::State *, int>, std::unordered_map<ob::State *, double>> querySMR(std::unordered_map<int, Graph> SMR, ob::State *goal, ob::State *OBS_STATE, double radius, ob::SpaceInformationPtr si) {
     // Build transition probability matrix
     std::unordered_map<int, std::map<ob::State *, std::map<ob::State *, double>>> prob;
      
@@ -214,7 +249,7 @@ std::tuple<std::unordered_map<ob::State *, int>, std::unordered_map<ob::State *,
     std::vector<ob::State *> &states = SMR[0].vertices;
     for (const auto &state : states) {
         values[state] = 0;
-        if (isGoalReachable(state, goal, radius)) {
+        if (isGoalReachable(state, goal, radius, si)) {
             reward[state] = 1;
         }
     }
@@ -227,6 +262,7 @@ std::tuple<std::unordered_map<ob::State *, int>, std::unordered_map<ob::State *,
     std::unordered_map<ob::State *, double> new_values;
     int iter = 0;
     double threshold = 1e-7;
+    double gamma = 0.00001; // penalty hyperparameter
     while (!stop && iter < max_iterations) {
         iter++;
         double max_change_in_value = 0;
@@ -249,7 +285,7 @@ std::tuple<std::unordered_map<ob::State *, int>, std::unordered_map<ob::State *,
                     // pair.second provides the probability from the current state to the next
                     ob::State *next = pair.first;
                     double probability = pair.second;
-                    exp_val += probability*values[next];
+                    exp_val += probability*(values[next]-gamma);
                 }
                 maxExpectedValue = std::max(maxExpectedValue, exp_val);
 
@@ -331,7 +367,7 @@ std::vector<ob::State *> extractPathFromPolicy(
         }
         
         // Get the best action for the current state
-        if (best_actions.find(current) == best_actions.end()) {
+        if (best_actions.find(current) == best_actions.end() && verbose) {
             std::cerr << "Error: No action for current state! Path computation aborted." << std::endl;
             break;
         }
@@ -342,7 +378,9 @@ std::vector<ob::State *> extractPathFromPolicy(
             
             
             for (const auto &edge : SMR[action].edges) {
-                if (edge.source == current && edge.target->as<ob::SE2StateSpace::StateType>()->getY()!=9999) {
+                double y;
+                std::tie(std::ignore, y, std::ignore) = getCoord(edge.target);
+                if (edge.source == current && y != 9999) {
                     next = const_cast<ob::State *>(edge.target); // Remove const for assignment
                 }
             }
@@ -388,7 +426,7 @@ std::vector<ob::State *> extractPathFromPolicy(
 std::tuple<int, int,int, bool> parseArguments(int argc, char* argv[]) {
     // Check if the required arguments are provided
     if (argc < 4) {
-        throw std::invalid_argument("Usage: ./program (sample size) (sample transitions) [--verbose]");
+        throw std::invalid_argument("Usage: ./program (sample size) (sample transitions) (policy option) [--verbose]");
     }
 
     int n, m, policy;
@@ -414,7 +452,7 @@ std::tuple<int, int,int, bool> parseArguments(int argc, char* argv[]) {
         throw std::invalid_argument("The third argument must be an integer (n).");
     }
     // Check for the optional verbose flag
-    if (argc > 3 && std::string(argv[4]) == "--verbose") {
+    if (argc > 4 && std::string(argv[4]) == "--verbose") {
         verbose = true;
     }
 
@@ -431,11 +469,16 @@ int main(int argc, char* argv[]) {
     std::cout << "n: " << n << ", m: " << m << ", verbose: " << (verbose ? "true" : "false") << std::endl;
     
     // Create space
-    auto space = std::make_shared<ob::SE2StateSpace>();
+    auto space = std::make_shared<NeedleStateSpace>();
     ob::RealVectorBounds bounds(2);
     bounds.setLow(-0.5);
     bounds.setHigh(.5);
-    space->setBounds(bounds);
+    space->getSubspace(0)->as<ob::SE2StateSpace>()->setBounds(bounds);
+    
+    ob::RealVectorBounds bBounds(1);
+    bBounds.setLow(0);
+    bBounds.setHigh(1);
+    space->getSubspace(1)->as<ob::RealVectorStateSpace>()->setBounds(bBounds);
 
     // Create space information
     auto si = std::make_shared<ob::SpaceInformation>(space);
@@ -445,13 +488,13 @@ int main(int argc, char* argv[]) {
     ob::State *start = si->allocState();
     ob::State *goal = si->allocState();
 
-    start->as<ob::SE2StateSpace::StateType>()->setX(-.5);
-    start->as<ob::SE2StateSpace::StateType>()->setY(-.4);
-    start->as<ob::SE2StateSpace::StateType>()->setYaw(0.0);
+    start->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setX(-.5);
+    start->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setY(-.4);
+    start->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setYaw(0.0);
 
-    goal->as<ob::SE2StateSpace::StateType>()->setX(.5);
-    goal->as<ob::SE2StateSpace::StateType>()->setY(.4);
-    goal->as<ob::SE2StateSpace::StateType>()->setYaw(0.0);
+    goal->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setX(.5);
+    goal->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setY(.4);
+    goal->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setYaw(0.0);
 
     // Create environment
     obstacles.clear();
@@ -462,8 +505,8 @@ int main(int argc, char* argv[]) {
 
     // Set obstacle state
     ob::State *OBS_STATE = si->allocState();
-    OBS_STATE->as<ob::SE2StateSpace::StateType>()->setX(9999);
-    OBS_STATE->as<ob::SE2StateSpace::StateType>()->setY(9999);
+    OBS_STATE->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setX(9999);
+    OBS_STATE->as<NeedleStateSpace::StateType>()->as<ob::SE2StateSpace::StateType>(0)->setY(9999);
 
     // Build SMR
     std::unordered_map<int, Graph> smr = buildSMR(si, start, goal, n, U, m, OBS_STATE);
@@ -474,10 +517,11 @@ int main(int argc, char* argv[]) {
             int u = roadmap.first;
             Graph graph = roadmap.second;
             for (const auto &e : graph.edges) {
-                auto source = e.source->as<ob::SE2StateSpace::StateType>();
-                auto target = e.target->as<ob::SE2StateSpace::StateType>();
-                std::string sStr = "(" + std::to_string(source->getX()) + ", " + std::to_string(source->getY()) + ")";
-                std::string tStr = "(" + std::to_string(target->getX()) + std::to_string(target->getY()) + ")";
+                double sx, sy, tx, ty;
+                std::tie(sx, sy, std::ignore) = getCoord(e.source);
+                std::tie(tx, ty, std::ignore) = getCoord(e.target);
+                std::string sStr = "(" + std::to_string(sx) + ", " + std::to_string(sy) + ")";
+                std::string tStr = "(" + std::to_string(tx) + ", " + std::to_string(ty) + ")";
                 std::cout << "Transition prob from " << sStr << " to " << tStr << " is " << e.prob << " with action u=" << u << std::endl;
                 if (e.target == goal) {
                     std::cout << "This path leads to the goal" << std::endl;
@@ -490,30 +534,32 @@ int main(int argc, char* argv[]) {
     std::unordered_map<ob::State *, int> best_actions;
     std::unordered_map<ob::State *, double> values;
     double radius = 0.1;
-    tie(best_actions, values) = querySMR(smr, goal, OBS_STATE, radius);
-    // std::ofstream valuesFile("values.txt");
-    // if (valuesFile.is_open()) {
-    //     for (auto *v: smr.at(0).vertices) {
-    //         double x = v->as<ob::SE2StateSpace::StateType>()->getX();
-    //         double y = v->as<ob::SE2StateSpace::StateType>()->getY();
-    //         valuesFile << x << "," << y << "," << values[v] << std::endl;
-    //     }
-    //     std::cout << "Written to values.txt" << std::endl;
-    // }
+    std::tie(best_actions, values) = querySMR(smr, goal, OBS_STATE, radius, si);
+    std::ofstream valuesFile("values.txt");
+    if (valuesFile.is_open()) {
+        for (auto *v: smr.at(0).vertices) {
+            double x, y;
+            std::tie(x, y, std::ignore) = getCoord(v);
+            valuesFile << x << "," << y << "," << values[v] << std::endl;
+        }
+        std::cout << "Written to values.txt" << std::endl;
+    }
 
     if (verbose) {
         std::cout << "Optimal policy obtained" << std::endl;
         for (const auto pair : best_actions) {
-            auto se2state = pair.first->as<ob::SE2StateSpace::StateType>();
-            std::cout << "Best action for state (" << std::to_string(se2state->getX()) << ", " << std::to_string(se2state->getY()) << ") is u=" << pair.second << std::endl;
+            double x, y;
+            std::tie(x, y, std::ignore) = getCoord(pair.first);
+            std::cout << "Best action for state (" << std::to_string(x) << ", " << std::to_string(y) << ") is u=" << pair.second << std::endl;
         }
 
         for (const auto pair : values) {
             if (pair.first != start) {
                 continue;
             }
-            auto se2state = pair.first->as<ob::SE2StateSpace::StateType>();
-            std::cout << "Probability for state (" << se2state->getX() << ", " << se2state->getY() << ") to reach goal: " << pair.second << std::endl;
+            double x, y;
+            std::tie(x, y, std::ignore) = getCoord(pair.first);
+            std::cout << "Probability for state (" << x << ", " << y << ") to reach goal: " << pair.second << std::endl;
         }
     }
 
@@ -523,7 +569,7 @@ int main(int argc, char* argv[]) {
     for (int k = 0; k < 1000; k++) {
         // Extract and print the path
         std::vector<ob::State *> path = extractPathFromPolicy(si, start, goal, best_actions, smr, verbose,policy);
-        if (!path.empty() && isGoalReachable(path.back(), goal, radius)) {
+        if (!path.empty() && isGoalReachable(path.back(), goal, radius, si)) {
             // This path reached the goal
             successCount++;
         }
@@ -531,10 +577,9 @@ int main(int argc, char* argv[]) {
         if (verbose) {
             std::cout << "Path from start to goal:" << std::endl;
             for (const auto &state : path) {
-                double x = state->as<ob::SE2StateSpace::StateType>()->getX();
-                double y = state->as<ob::SE2StateSpace::StateType>()->getY();
-                double yaw = state->as<ob::SE2StateSpace::StateType>()->getYaw();
-                std::cout << "(" << x << ", " << y << ", " << yaw << ")" << std::endl;
+                double x, y, theta;
+                std::tie(x, y, theta) = getCoord(state);
+                std::cout << "(" << x << ", " << y << ", " << theta << ")" << std::endl;
             }
         }
     }
