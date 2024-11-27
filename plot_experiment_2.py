0dd4aaf4eb306092cb33ac040cee4de33abd32b4
Joshua Han <joshhan619@gmail.com>
2024-11-27 09:58:05 -0600

    chg: Updated experiment plot script

--- plot_experiment_2.py
+++ plot_experiment_2.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import make_interp_spline
 
 if __name__ == '__main__':
-    means = np.zeros(3)
-    sds = np.zeros(3)
-    sample_sizes = [1000, 10000, 100000]
+    means = np.zeros(4)
+    sds = np.zeros(4)
+    sample_sizes = [1000, 10000, 100000, 1000000]
     for index, n in enumerate(sample_sizes):
         data = []
         with open(f"output{n}.txt") as f:
@@ -13,15 +14,20 @@ if __name__ == '__main__':
                 data.append(float(line))
         data = np.array(data)
         means[index] = np.mean(data)
-        sds[index] = np.sqrt(np.sum((data - means[index]))**2/20)
+        sds[index] = np.std(data)
 
+    log_sample_sizes = np.log10(sample_sizes)
+    spline = make_interp_spline(log_sample_sizes, means)
 
-    print(means, sds)
+    x = np.linspace(3, 6, 500)
+    y = spline(x)
+
     fig, ax = plt.subplots()
-    ax.errorbar(np.log10(sample_sizes), means, sds, linestyle='None', fmt='o')
-    ax.set_ylabel("Mean difference: Actual vs Expected Probability")
+    ax.errorbar(log_sample_sizes, means, sds, fmt='o')
+    ax.plot(x, y)
+    ax.set_ylabel("Mean Difference in Probability")
     ax.set_xlabel("Log(Sample Size)")
-    ax.set_title("Experiment 2")
+    ax.set_title("Mean difference: Actual vs Expected Probability")
     plt.show()
 
     
